6942ce9e7eefd9dc91bb2bc491f7f1d7ee8c100b
Clay <headwig2435@gmail.com>
2024-05-29 19:35:33 +1200

    implementing parts of frountend gui

---
+++ CourseEnrolmentGUI/src/main/java/com/spcr/courseenrolmentgui/AddRemovePanel.java
@@ -0,0 +1,22 @@
+/**
+ * @author Clayton
+ */
+package com.spcr.courseenrolmentgui;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class AddRemovePanel extends JPanel{
+        public AddRemovePanel() {
+            setLayout(new GridLayout(1, 2));
+            JToggleButton addButton = new JToggleButton("Add");
+            JToggleButton removeButton = new JToggleButton("Remove");
+            ButtonGroup group = new ButtonGroup();
+            group.add(addButton);
+            group.add(removeButton);
+            addButton.setSelected(true);
+
+            add(addButton);
+            add(removeButton);
+    }
+}
