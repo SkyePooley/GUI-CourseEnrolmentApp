39e479c3a41ca3dedac7c7154c812172ac04d335
Skye Pooley <tpb6375@autuni.ac.nz>
2024-06-10 13:40:23 +1200

    Fixed login error not showing
    LoginView was not being updated as I forgot to add it to the model as an observer.
    Rectified this issue and changed the error message.

--- CourseEnrolmentGuiAnt/src/GUI/CourseEnrolmentGUI.java
+++ CourseEnrolmentGuiAnt/src/GUI/CourseEnrolmentGUI.java
@@ -1,5 +1,5 @@
 /**
- * Application for students to add and remove enrolments from their schedules.
+ * Application for students to add and remove enrollments from their schedules.
  * @author Skye Pooley & Clayton Roberts
  */
 package GUI;
@@ -18,6 +18,7 @@ public class CourseEnrolmentGUI {
 
         View mainWindow = new View();
         model.addObserver(mainWindow);
+        model.addObserver(mainWindow.loginPopup);
 
         Controller controller = new Controller();
         controller.addModel(model);
